bf672f1a2795b3e84433cb9e95c7b1e5c17bab28
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-08-25 09:57:52 +0300

    fixed: slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 new WOW().init();
 const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
-  spaceBetween: 1000,
+  spaceBetween: 1500,
   pagination: {
     el: ".projects-pagination",
     bulletClass: "projects-bullet",
