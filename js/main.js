4657aab6ab29926fac76e0633816e37d38cd7f30
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-08-25 09:48:49 +0300

    create: slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 new WOW().init();
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 1000,
+  pagination: {
+    el: ".projects-pagination",
+    bulletClass: "projects-bullet",
+    bulletActiveClass: "projects-bullet-active",
+    clickable: true,
+  },
+});
